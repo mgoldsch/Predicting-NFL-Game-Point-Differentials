96dc396a02014d6bbe84ca889657ceead7bd5d92
Michael Goldschlager <michaelgoldschlager@Michaels-MacBook-Air.local>
2022-02-20 01:12:28 -0500

    added logic for getting time of posession and average per drive, play count and average plays per drive. can't join the posession stats b/c it is a df and setting copy=TRUE on the merge is causing a parser stack overflow. idea: write combinded table to db, write posession df to db as table and merge there and then delete the posession db

--- data_set_creation.R
+++ data_set_creation.R
@@ -25,23 +25,62 @@ game_team_yac <- pbp_db %>%
   dplyr::group_by(game_id, posteam) %>%
   dplyr::summarise(total_yac = sum(yards_after_catch, na.rm = TRUE))
 
+#calculate the total time of possession and average time of possession of a drive for each team for a game
+game_team_pos_total_avg <- pbp_db %>%
+  dplyr::filter(!is.na(posteam) & posteam != "") %>%
+  dplyr::group_by(game_id, posteam, fixed_drive) %>%
+  dplyr::summarise(drive_time_of_possession = drive_time_of_possession) %>%
+  dplyr::ungroup() %>%
+  dplyr::collect()
+game_team_pos_total_avg$drive_time_of_possession_in_sec <- lubridate::period_to_seconds(lubridate::ms(game_team_pos_total_avg$drive_time_of_possession))
+game_team_pos_total_avg <- game_team_pos_total_avg %>%
+  dplyr::group_by(game_id, posteam) %>%
+  dplyr::summarise(total_time_pos = sum(drive_time_of_possession_in_sec, na.rm = TRUE), avg_drive_time_pos = mean(drive_time_of_possession_in_sec, na.rm = TRUE)) %>%
+  dplyr::select(game_id, posteam, total_time_pos, avg_drive_time_pos)
+
+#calculate total play count and average play count per drive for each team of a game
+game_team_play_count_avg <- pbp_db %>%
+  dplyr::filter(!is.na(posteam) & posteam != "") %>%
+  dplyr::group_by(game_id, posteam, fixed_drive) %>%
+  dplyr::summarise(drive_play_count = drive_play_count) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(game_id, posteam) %>%
+  dplyr::summarise(total_plays = sum(drive_play_count), avg_plays_per_drive = mean(drive_play_count)) %>%
+  dplyr::select(game_id, posteam, total_plays, avg_plays_per_drive)
+
+
 #get the teams in each game and other info like the point differential and scores of the teams
 game_teams_and_info <- pbp_db %>%
   dplyr::group_by(game_id) %>%
   dplyr::summarise(home_team = home_team, away_team = away_team, home_score = home_score, away_score = away_score, result = result) %>%
-  dplyr::select(game_id, home_team, away_team, home_score, away_score, result) %>% 
+  dplyr::select(game_id, home_team, away_team, home_score, away_score, result) %>%
   dplyr::rename(point_differential = result)
 
 #produce a result that has the info from game_teams_and_info and the offensive yards for each team and yac
 combined <- game_teams_and_info %>%
+  #add offensive yards
   dplyr::left_join(game_team_offense_yards, by = c('game_id' = 'game_id', 'home_team' = 'posteam')) %>%
   rename(home_off_yards = off_yards) %>%
   dplyr::left_join(game_team_offense_yards, by = c('game_id' = 'game_id', 'away_team' = 'posteam')) %>%
   rename(away_off_yards = off_yards) %>%
+  #add yac
   dplyr::left_join(game_team_yac, by = c('game_id' = 'game_id', 'home_team' = 'posteam')) %>%
   rename(home_yac = total_yac) %>%
   dplyr::left_join(game_team_yac, by = c('game_id' = 'game_id', 'away_team' = 'posteam')) %>%
-  rename(away_yac = total_yac)
+  rename(away_yac = total_yac) %>% 
+  #add time of possession stats
+  dplyr::left_join(game_team_pos_total_avg, by = c('game_id' = 'game_id', 'home_team' = 'posteam'), copy = TRUE) %>%
+  rename(home_total_time_pos = total_time_pos, home_avg_drive_time_pos = avg_drive_time_pos) %>%
+  dplyr::left_join(game_team_pos_total_avg, by = c('game_id' = 'game_id', 'away_team' = 'posteam'), copy = TRUE) %>%
+  rename(away_total_time_pos = total_time_pos, away_avg_drive_time_pos = avg_drive_time_pos) %>%
+  #i believe the above can't be joined b/c it is a dataframe but the rest is sqlite query- confired
+  #setting copy=TRUE allows you to get around this but I think there is a memory issue....
+  #try writing this df to db as a table, then join them in the db and delete?
+  #add play count and average play count per drive
+  dplyr::left_join(game_team_play_count_avg, by = c('game_id' = 'game_id', 'home_team' = 'posteam')) %>%
+  rename(home_total_plays = total_plays, home_plays_per_drive = avg_plays_per_drive) %>%
+  dplyr::left_join(game_team_play_count_avg, by = c('game_id' = 'game_id', 'away_team' = 'posteam')) %>%
+  rename(away_total_plays = total_plays, away_plays_per_drive = avg_plays_per_drive)
 
 combined
 
